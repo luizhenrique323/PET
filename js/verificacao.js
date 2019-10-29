280517b7ddfba96c0b999f4ccaf727d7f4a2f175
Leonardo <Leonardo13022019@gmail.com>
2019-10-29 14:50:46 -0300

    Finalização da criação do Layout da tela de cadastro

--- js/verificacao.js
+++ js/verificacao.js
@@ -2,26 +2,53 @@ $(document).ready(function(){
   $("#confEmail").hide();
   $("#confPassword").hide();
 
-  $("#iInputConfEmail").focusout(function(){
+  $("#iInputConfEmail, #iInputEmail").focusout(function(){
     var email = $("#iInputEmail").val();
     var confEmail = $("#iInputConfEmail").val();
-    if (email != confEmail) {
-    	$("#confEmail").show();
-    }
-    else {
-    	$("#confEmail").hide();
+    if (email != '' && confEmail != '') {
+      if (email != confEmail) {
+        $("#confEmail").show();
+      }
+      else {
+        $("#confEmail").hide();
+      }
     }
   });
 
-  $("#iInputConfPassword").focusout(function(){
+  $("#iInputConfPassword, #iInputPassword").focusout(function(){
   	var pass = $("#iInputPassword").val();
     var confPass = $("#iInputConfPassword").val();
-    if (pass != confPass) {
-    	$("#confPassword").show();
+    if (pass != '' && confPass != '') {
+      if (pass != confPass) {
+      	$("#confPassword").show();
+      }
+      else {
+      	$("#confPassword").hide();
+      }
     }
-    else {
-    	$("#confPassword").hide();
+  });
+
+ 
+  $("#iInputPhone").keypress(function( event ){
+    var phone  = $(this).val();
+    var qtd    = phone.length;
+    var index  = qtd - 1;
+    var key = event.which; 
+
+    if (!((key >= 48) && (key <= 57))) {
+        return false;
     }
+
+    if (index == 0) {
+      $(this).val('(' + phone);
+    }
+    else if (index == 2) {
+      $(this).val(phone + ')');
+    }
+    else if (index == 8) {
+      $(this).val(phone + '-');
+    }
+    
   });
 });
 
